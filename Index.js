78bb55f8cc5723122c0716912dd8b30471e01c5d
pabloarieliglesias@gmail.com <pabloarieliglesias@gmail.com>
2024-02-16 12:38:52 -0300

    Laboratorio 2

--- Index.js
+++ Index.js
@@ -1,1 +1,15 @@
 console.log("Hola mundo Node.JS");
+
+/**
+ * Adds two numbers together.
+ *
+ * @param {number} num1 - The first number to be added
+ * @param {number} num2 - The second number to be added
+ * @return {number} The sum of num1 and num2
+ */
+function addNumbers(num1, num2) {
+    console.log("La suma de los numeros es: " + (num1 + num2));
+    return num1 + num2;
+}
+
+addNumbers(9, 3);
